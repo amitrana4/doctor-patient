127205cc57c4132dc2e4c3af55bb449168e2c812
CLICKLABS147 <clicklabs147@CLICKLABS147s-Mac-mini.local>
2016-04-13 13:31:53 +0530

    db changes

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var mongo = {
-    URI: process.env.MONGO_URI || 'mongodb://localhost/givapp',
+    URI: process.env.MONGO_URI || 'mongodb://localhost/givapp_dev',
+    user: 'givapp_dev',
+    pass: 'cNUaaaF3f2HclDY',
     port: 27017
+
 };
 
 
