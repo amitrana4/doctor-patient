ed8d59fa4f5daba874de7871fa070dd61def25e6
CLICKLABS147 <clicklabs147@CLICKLABS147s-Mac-mini.local>
2016-04-21 11:42:17 +0530

    edit campaign done

--- Routes/CharityRoute.js
+++ Routes/CharityRoute.js
@@ -397,7 +397,7 @@ module.exports = [
             },
             plugins: {
                 'hapi-swagger': {
-                    payloadType: 'form',
+                    //payloadType: 'form',
                     responseMessages: UniversalFunctions.CONFIG.APP_CONSTANTS.swaggerDefaultResponseMessages
                 }
             }
