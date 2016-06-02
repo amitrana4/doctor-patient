e8f3e5579626a2debfe36c1afcc1e49740f65562
cl-macmini-147 <cl-macmini-147@cl-macmini-147.local>
2016-06-02 10:44:20 +0530

    payment api for admin

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ var schedule = require('node-schedule');
 
 var rule = new schedule.RecurrenceRule();
 
-rule.minute = 13;
+rule.minute = 16;
 
 var j = schedule.scheduleJob(rule, function(){
     Controller.DonorController.cronRecurringDonationCampaign(function (err, data) {
